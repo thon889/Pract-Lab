37f024278e92c5088be995acc9d2f47d807a71cf
thon889 <thon889@aucklanduni.ac.nz>
2017-06-01 12:43:15 +1200

    Add files via upload
    Uploading unity files from my practice test.

---
+++ Sketch.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using Pathfinding.Serialization.JsonFx; //make sure you include this using
+
+public class Sketch : MonoBehaviour {
+    public GameObject myPrefab;
+    //string DataSeat = "treesurveyv3"; 
+    // Put your URL here
+    //public 
+    string _WebsiteURL = "http://infomgmt192.azurewebsites.net/tables/" + "movies" + "?zumo-api-version=2.0.0";
+
+    void Start () {
+        //Reguest.GET can be called passing in your ODATA url as a string in the form:
+        //http://{Your Site Name}.azurewebsites.net/tables/{Your Table Name}?zumo-api-version=2.0.0
+        //The response produce is a JSON string
+//        string Dataseat = "treesurveyv3";
+        string jsonResponse = Request.GET(_WebsiteURL);
+
+        //Just in case something went wrong with the request we check the reponse and exit if there is no response.
+        if (string.IsNullOrEmpty(jsonResponse))
+        {
+            return;
+        }
+
+        //We can now deserialize into an array of objects - in this case the class we created. The deserializer is smart enough to instantiate all the classes and populate the variables based on column name.
+        Movie[] movies = JsonReader.Deserialize<Movie[]>(jsonResponse);
+
+        int totalCubes = movies.Length;
+        int totalDistance = 5;
+        int i = 0;
+        //We can now loop through the array of objects and access each object individually
+        foreach (Movie movie in movies)
+        {
+            //Example of how to use the object
+            Debug.Log("This products name is: " + movie.MovieID);
+            float perc = i / (float)totalCubes;
+            i++;
+            float x = perc * totalDistance;
+            float y = 5.0f;
+            float z = 0.0f;
+            GameObject newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
+            newCube.GetComponent<myCubeScript>().setSize((1.0f - perc) * 2);
+            newCube.GetComponent<myCubeScript>().ratateSpeed = perc;
+            newCube.GetComponentInChildren<TextMesh>().text = movie.MovieID;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ TreeSurvey.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//Make sure you get rid of the namespace that is automatically generated here.
+class Movie
+{
+    //List all the columns of your table here in the same format as below.
+    public string MovieID { get; set; }
+
+    public string id { get; set; }
+    public string createdAt { get; set; }
+    public string updatedAt    { get; set; }
+    public string version    { get; set; }
+
+    public string IMDBURL    { get; set; }
+
+    public string Action { get; set; }
+    public string Comedy { get; set; }
+    public string Childrens { get; set; }
+    public string Documentary { get; set; }
+    public string Drama { get; set; }
+    public string Fantasy { get; set; }
+    public string Horror { get; set; }
+    public string Mystery { get; set; }
+
+    public string Romance { get; set; }
+
+   
+    public string Thriller { get; set; }
+
+
+
+}
